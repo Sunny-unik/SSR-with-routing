788429464bc2f19b781108c6846038b56c23f254
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-25 22:43:26 +0530

    feat: add grid component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,19 +1,6 @@
 import React from "react";
+import Grid from "./components/Grid.jsx";
 
 export default function App({ data }) {
-  return (
-    <ul style={{ display: "flex", flexWrap: "wrap" }}>
-      {data.map(({ name, owner, stargazers_count, html_url }) => (
-        <li key={name} style={{ margin: 30 }}>
-          <ul>
-            <li>
-              <a href={html_url}>{name}</a>
-            </li>
-            <li>@{owner.login}</li>
-            <li>{stargazers_count} stars</li>
-          </ul>
-        </li>
-      ))}
-    </ul>
-  );
+  return <Grid repos={data} />;
 }
