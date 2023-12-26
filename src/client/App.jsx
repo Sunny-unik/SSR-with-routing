ce8b361ed1c4f62f192513550557dfe66ba57461
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-27 01:00:11 +0530

    feat: improve home.jsx, all navigations & styles

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import routes from "../shared/routes";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
 
 export default function App({ data }) {
   return (
-    <Routes>
-      {routes.map(({ path, component: C, ...rest }) => (
-        <Route key={path} path={path} element={<C {...rest} repos={data} />} />
-      ))}
-    </Routes>
+    <div className="container">
+      <h1>Select a language to show popular github repositories</h1>
+      <Navbar />
+      <Routes>
+        {routes.map(({ path, component: C, ...rest }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<C {...rest} repos={data} />}
+          />
+        ))}
+      </Routes>
+    </div>
   );
 }
--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,11 +1,47 @@
-import React from "react";
-import Navbar from "./Navbar.jsx";
+import React, { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
+  const egLanguages = [
+    "ruby",
+    "bash",
+    "go",
+    "perl",
+    "php",
+    "csharp",
+    "c",
+    "rust",
+  ];
+  let languageIndex = 0;
+  const [language, setLanguage] = useState(egLanguages[languageIndex]);
+
+  const nextLanguageIndex = function* () {
+    while (true) {
+      yield languageIndex < egLanguages.length - 1
+        ? (languageIndex += 1)
+        : (languageIndex = 0);
+    }
+  };
+
+  const indexGenerator = useMemo(() => nextLanguageIndex(), []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLanguage(egLanguages[indexGenerator.next().value]);
+    }, 1500);
+
+    return () => clearInterval(intervalId);
+  }, [egLanguages, indexGenerator]);
+
   return (
-    <>
-      <h1>Select a language to show popular github repositories</h1>
-      <Navbar />
-    </>
+    <div>
+      <p>
+        If you can't find your preferred language in this list then you can
+        navigate by adding your language in url with followed by popular route.
+      </p>
+      <p>For Example:</p>
+      <code>
+        https://domain/<b>popular/{language}/</b>
+      </code>
+    </div>
   );
 }
