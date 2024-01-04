6435ede0ed9b2efa36a18ccc27f7cc4195a4ab66
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-01-05 00:09:21 +0530

    chore: improve static-files management

--- assets.config.js
+++ assets.config.js
@@ -1,12 +1,13 @@
 const fsExtra = require("fs-extra");
-const fs = require("node:fs");
+const { copyFile } = require("node:fs");
 
 fsExtra
   .copy("public", "dist/public")
-  .then(() => console.log("Public directory copied successfully!"))
+  .then(() => {
+    console.log("Public directory copied successfully!");
+    copyFile("vercel.json", "dist/vercel.json", (error) => {
+      if (error) return console.log(error);
+      console.log("vercel.json copied successfully!");
+    });
+  })
   .catch((err) => console.error(err));
-
-fs.copyFile("vercel.json", "dist/vercel.json", (error) => {
-  if (error) return console.log(error);
-  console.log("vercel.json copied successfully!");
-});
