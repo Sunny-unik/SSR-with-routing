31f7c828985aece88e958e2e51e56105ec66a362
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-01-04 17:48:26 +0530

    fix: update build config

--- assets.config.js
+++ assets.config.js
@@ -1,5 +1,12 @@
-const fs = require("fs-extra");
+const fsExtra = require("fs-extra");
+const fs = require("node:fs");
 
-fs.copy("public", "dist/public")
+fsExtra
+  .copy("public", "dist/public")
   .then(() => console.log("Public directory copied successfully!"))
   .catch((err) => console.error(err));
+
+fs.copyFile("vercel.json", "dist/vercel.json", (error) => {
+  if (error) return console.log(error);
+  console.log("vercel.json copied successfully!");
+});
