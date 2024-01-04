ad2a0677482fcc03113a61a0b3744349d51a51d4
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-01-04 20:19:13 +0530

    fix: replace isomorphic-fetch from node@18 fetch
    to fix n.call is not defined error on production

--- src/shared/githubAPI.js
+++ src/shared/githubAPI.js
@@ -1,10 +1,8 @@
-import _fetch from "isomorphic-fetch";
-
 export function fetchPopularRepos(language = "all") {
   const encodedURI = encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1000+language:${language}&sort=stars&order=desc&type=Repositories`
   );
-  return _fetch(encodedURI)
+  return fetch(encodedURI)
     .then((data) => data.json())
     .then((repos) => repos.items)
     .catch((error) => {
